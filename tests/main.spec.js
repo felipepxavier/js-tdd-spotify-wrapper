bce9b785da07a0ec6f3758b2bc99ae0903364a47
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2021-03-08 15:02:33 -0300

    feat: initial functions search

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,24 @@
 import { expect } from 'chai';
+import {
+  search, searchAlbums, searchArtists, searchTracks, searchPlaylists,
+} from '../src/main';
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
+  describe('smoke tests', () => {
+    // search (generico)
+    // searchAlbums
+    // searchArtists
+    // searchTracks
+    // searchPlaylists
 
+    it('Should exist the search method', () => expect(search).to.exist);
+
+    it('Should exist the searchAlbums method', () => expect(searchAlbums).to.exist);
+
+    it('Should exist the searchArtists method', () => expect(searchArtists).to.exist);
+
+    it('Should exist the searchTracks method', () => expect(searchTracks).to.exist);
+
+    it('Should exist the searchPlaylists method', () => expect(searchPlaylists).to.exist);
+  });
 });
