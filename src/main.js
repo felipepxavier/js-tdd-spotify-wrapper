ae8129006699b886ed67520dc85bc284b6452dd2
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2021-03-09 16:56:33 -0300

    fix: ajust function main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-export const search = (query, type) => {
-  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`);
-};
-export const searchAlbums = () => {};
-export const searchArtists = () => {};
-export const searchTracks = () => {};
-export const searchPlaylists = () => {};
+export const search = (query, type) => fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`, { headers: { Authorization: 'Bearer BQByup669rZdifivc0YFqj8K6DinYNI-S4BUIBK0JnJiwee_bGNWCCj5Utp0kjro4KEWrPAxsywxCH1nSAVJWMDdze2PweAMWjhbQJMJjjEPOVfnBVY2--GFpTOAZcj3PvZiMPQRcRcaBlHf6h-hBguY3NvMHaXwwTk' } })
+  .then((data) => data.json());
+
+export const searchArtists = (query) => search(query, 'artist');
+export const searchAlbums = (query) => search(query, 'album');
+export const searchTracks = (query) => search(query, 'track');
+export const searchPlaylists = (query) => search(query, 'playlist');
