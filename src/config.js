6576241f5286847afaec9f905619434b87e8378c
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2021-03-12 11:21:48 -0300

    chore: config webpack

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const TOKEN_API = 'BQDkwsMQ0mCRk9BP8QysJ4lyMhBaHP15OAOzh0ir7FX6HzBkQiTLdgeARrKlh9tcJAKFF9xRPn3f2TNKv-ZhPjRU8f641zBEiC-tTfhtYe1Fnsoqb2cXs0WwspzJulCk4wKPLVQbTbHndRmFlXfloFeVsapVkZ3wCZY';
+
+export const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${TOKEN_API}`,
+  },
+
+};
+export const API_URL = 'https://api.spotify.com/v1';
